39b53c075596e3676e14632aeff9d16b69635f46
Abhishek0721 <96063426+Abhishek0721@users.noreply.github.com>
2022-05-31 23:53:12 +0530

    Add files via upload
    This is the first video(multiple components)

---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+
+
+
+    const NavBar= () => {
+
+        return (
+          <nav className="navBar2">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+              <a href="/">Home</a>
+              
+            </div>
+          </nav>
+        );
+      }
+       
+      export default NavBar;
+      
+          
+
+
+    
